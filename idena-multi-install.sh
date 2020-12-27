282424d0eac9b2c961da6a85f0c76857da5e9ff1
znyber <pro_army@live.com>
2020-12-27 15:42:28 +0700

    update for telgram bot & install node share

---
+++ idena-multi-install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+idenahome="$1"
+idenanumber="$2"
+idenakeystore="$3"
+sed -n "1{p;q}" api.txt >> $idenahome.txt 
+portRpc=$(tail -1 $idenanumber-rpc.txt)
+portIpf=$(tail -1 $idenanumber-ipf.txt)
+sed -i "1d" api.txt
+if [ ! -d /home/$idenahome/$idenanumber ]
+then
+	mkdir -p /home/$idenahome/$idenanumber
+	idena_download=$(curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep linux | cut -d '"' -f 4 | head -n 2 | tail -n 1)
+
+	sed -n "1{p;q}" api.txt >> $idenahome.txt 
+	portRpc=$(tail -1 $idenanumber-rpc.txt)
+	portIpf=$(tail -1 $idenanumber-ipf.txt)
+	sed -i "1d" api.txt
+	#------------ download idena node latest version--------------#
+
+	wget $idena_download -q --show-progress -O /usr/bin/idena
+	wget https://raw.githubusercontent.com/znyber/idena-installer/master/idena-update -q --show-progress -O /usr/bin/idena-update
+	chmod +x /usr/bin/idena
+	chmod +x /usr/bin/idena-update
+	#--------------- create idena service-------------------------#
+
+cat <<EOF > /lib/systemd/system/idena-$idenahome-$idenanumber.service
+[Unit]
+Description=idena $idenahome service
+After=network.target
+StartLimitIntervalSec=0
+
+[Service]
+Restart=always
+RestartSec=1
+WorkingDirectory=/home/$idenahome/$idenanumber
+User=root
+ExecStart=-/usr/bin/idena --profile=lowpower --rpcaddr=localhost --rpcport=
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable idena-$idenahome-$idenanumber
+service idena-$idenahome-$idenanumber start
+
+echo "wait.... build datadir"
+sleep 30
+echo $idenakeystore > /home/$idenahome/$idenanumber/datadir/keystore/nodekey
+echo $idenahome > /home/$idenahome/$idenanumber/datadir/api.key
+service idena stop
+systemctl daemon-reload
+rm -rf /home/$idenahome/$idenanumber/datadir/idenachain.db/*
+mount --bind /home/datadir/idenachain.db /home/$idenahome/$idenanumber/datadir/idenachain.db
+cat <<EOF >> /etc/fstab
+/home/datadir/idenachain.db /home/$idenahome/$idenanumber/datadir/idenachain.db none bind
+EOF
+rm -rf /home/$idenahome/$idenanumber/datadir/ipfs/*
+systemctl daemon-reload
+service idena start
+echo " Copy this API key to your idena client "
+cat /home/$idenahome/$idenanumber/datadir/api.key && echo ''
+else
+    echo "user node $idenahome sudah ada mohon ganti"
+    echo "ex:" $idenahome"1" $idenahome"2" $idenahome"3" "dst.."
+fi
---
+++ index.js
@@ -0,0 +1,60 @@
+const Telegraf = require('telegraf')
+const bot = new Telegraf('1422705649:AAFJWFkgiHRRE5K72XtV_1gU4lKHROa9nZI')
+const { exec } = require('child_process');
+const fs = require('fs');
+const readline = require('readline');
+const { Validator } = require('node-input-validator');
+
+bot.on('text', (ctx) => {
+console.log(ctx.message)
+
+if (ctx.message.text == '/start') {
+ctx.replyWithHTML(
+'<i>oi bro</i> @<b>'+ ctx.from.username +'</b> \n bot kie nggo gawe node shared api \n nek ora ko bisa melu masang mining lewat bot kie, \n carane PM bae bot kie \n terus ketik bae /tulung \n nko di njlentrehna carane kepriwe \n nek ora mudeng PM sing due bae ',
+{'reply_to_message_id':ctx.message.message_id})}
+
+if (ctx.message.text == '/tulung') {
+ctx.replyWithMarkdown('kie nggo ko @*'+ ctx.from.username +'* \n /newapi - nggo gawe random api \n /listapi kie nggo ndeleng kabeh apikey sing ws pernah ko gawe \n /nodekey - nek pengin masang nodekey ben bsa melu mining neng kene',
+{'reply_to_message_id':ctx.message.message_id})}
+
+if (ctx.message.text == '/newapi') {
+exec('sed -n "1{p;q}" api.txt >>'+ ctx.from.username +'.txt && tail -1 '+ ctx.from.username +'.txt && sed -i "1d" api.txt', (error, stdout, stderr) => {
+    if (error) {
+        ctx.reply(`error: ${error.message}`);
+    }
+    if (stderr) {
+        ctx.reply(`stderr: ${stderr}`);
+    }
+    ctx.reply(`api mu`);
+    ctx.reply(`${stdout}`);
+});}
+if (ctx.message.text == '/listapi') {
+async function processLineByLine() {
+  const fileStream = fs.createReadStream(''+ ctx.from.username +'.txt');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  for await (const line of rl) {
+    console.log(ctx.reply(`${line}`));
+  }
+}
+
+processLineByLine();}
+
+if (ctx.message.text == '/nodekey' && ctx.chat.type == 'private') {
+exec('cat ./znyber.txt', (error, stdout, stderr) => {
+    if (error) {
+        ctx.reply(`error: ${error.message}`);
+    }
+    if (stderr) {
+        ctx.reply(`stderr: ${stderr}`);
+    }
+    ctx.reply(`api mu ${ctx.message.text}`);
+    ctx.reply(`${stdout}`);
+});}
+else{ ctx.reply(`lewat PM ae , kui ana sing salah kodene ko`) }
+})
+bot.startPolling()
