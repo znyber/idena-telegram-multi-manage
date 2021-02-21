4ddf94f5ec5943252d80d8177d5f49f7b4908640
znyber <pro_army@live.com>
2021-02-22 03:56:05 +0700

    ceklek-htm

--- ceklek-htm.sh
+++ ceklek-htm.sh
@@ -10,20 +10,20 @@ cat <<EOF > /home/all.html
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic&amp;display=swap">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
-	<style type="text/css">
-	.abang {
-	background-color:Tomato;
-	}
-	.ijo {
-	background-color:MediumSeaGreen;
-	}
-	#table_detail .hidden_row { 
-			display: none; 
-	} 
-	.square {
+        <style type="text/css">
+        .abang {
+        background-color:Tomato;
+        }
+        .ijo {
+        background-color:MediumSeaGreen;
+        }
+        #table_detail .hidden_row {
+                        display: none;
+        }
+        .square {
     width: 60px;
-	}
-	</style>
+        }
+        </style>
 </style>
 </head>
 
@@ -37,14 +37,14 @@ cat <<EOF > /home/all.html
                 <th>Show More</th>
             </tr>
         </thead>
-		<tbody>
+                <tbody>
 EOF
 while read line; do
 cat <<EOF >> /home/all.html
-			<tr style="background-color:DodgerBlue;">
-				<td>User</td>
+                        <tr style="background-color:DodgerBlue;">
+                                <td>User</td>
                 <td colspan="4"><h3>$line</h3></td>
-			</tr>
+                        </tr>
 EOF
 while read line2; do
 KOVET=$(netstat -netulp |grep $line2 | awk -F "[ :]+" '/:/{print $5}')
@@ -57,7 +57,7 @@ age=$(echo $DATC | sed -n 's|.*"age":\([^"]*\),.*|\1|p')
 add1=$(echo $addrss | head -c 3)
 add2=$(echo $addrss | tail -c 3)
 minir=$(echo $DATC | sed -n 's|.*"online":\([^"]*\),.*|\1|p')
-IC=$(echo $DATC | sed -n 's|.*"invites":"\([^"]*\),.*|\1|p')
+IC=$(echo $DATC | sed -n 's|.*"invites":\([^"]*\),.*|\1|p')
 BALC=$(curl -s "http://127.0.0.1:$line2/" -H 'Content-Type: application/json' --data "{\"method\":\"dna_getBalance\",\"params\":[\"$addrss\"],\"id\":1,\"key\":\"$line\"}")
 stuck=$(echo $BALC | sed -n 's|.*"stake":"\([^"]*\)".*|\1|p')
 wllt=$(echo $BALC | sed -n 's|.*"balance":"\([^"]*\)".*|\1|p')
@@ -70,57 +70,63 @@ online="OFF"
 logon="⛔"
 fi
 cat <<EOF >> /home/all.html
-				<tr>
-				<td><a href="https://scan.idena.io/identity/$addrss" target="_blank">$add1...$add2</a></td>
+                                <tr>
+                                <td><a href="https://scan.idena.io/identity/$addrss" target="_blank">$add1...$add2</a></td>
                 <td>$states</td>
                 <td class="ijo"><h5>$line2</h5></td>
                 <td>ON</td>
                 <td><button onclick="showHideRow('hidden_row$line2');">show more</button></td>
-				</tr>
-				<tr id="table_detail" >
-				<td id="hidden_row$line2" class="hidden_row" colspan="5">
-				<table class="table table-striped" cellspacing="0" width="100%">
-					<tbody>
-						<tr>
-							<td rowspan="2" class="square" colspan="1"><img class="square" src="https://robohash.idena.io/$addrss"/></td>
-							<td colspan="4"><h5>$states</h5></td>
-						</tr>
-						<tr>
-							<td colspan="3">
-								$addrss
-							</td>
-							<td>Mining : $logon</td>
-						</tr>
-						<tr>
-							<td>Address :</td>
-							<td colspan="4"><a href="https://scan.idena.io/identity/$addrss" target="_blank">$addrss</a></td>
-						</tr>
-						<tr>
-							<td>Status :</td>
-							<td colspan="4">$states</td>
-						</tr>
-						<tr>
-							<td>Balance :</td>
-							<td colspan="4">$wllt</td>
-						</tr>
-						<tr>
-							<td>Stake :</td>
-							<td colspan="4">$stuck</td>
-						</tr>
-						<tr>
-							<td>Age : </td>
-							<td colspan="4">$age</td>
-						</tr>
-					</tbody>
-				</table>
-				</td>
-				</tr>
+                                </tr>
+                                <tr id="table_detail" >
+                                <td id="hidden_row$line2" class="hidden_row" colspan="5">
+                                <table class="table table-striped" cellspacing="0" width="100%">
+                                        <tbody>
+                                                <tr>
+                                                        <td rowspan="2" class="square" colspan="1"><img class="square" src="https://robohash.idena.io/$addrss"/></td>
+                                                        <td colspan="4"><h5>$states</h5></td>
+                                                </tr>
+                                                <tr>
+                                                        <td colspan="3">
+                                                                $addrss
+                                                        </td>
+                                                        <td>Mining : $logon</td>
+                                                </tr>
+                                                <tr>
+                                                        <td>Address :</td>
+                                                        <td colspan="4"><a href="https://scan.idena.io/identity/$addrss" target="_blank">$addrss</a></td>
+                                                </tr>
+                                                <tr>
+                                                        <td>Status :</td>
+                                                        <td colspan="4">$states</td>
+                                                </tr>
+                                                <tr>
+                                                        <td>Balance :</td>
+                                                        <td colspan="4">$wllt</td>
+                                                </tr>
+                                                <tr>
+                                                        <td>Stake :</td>
+                                                        <td colspan="4">$stuck</td>
+                                                </tr>
+                                                <tr>
+                                                        <td>Age : </td>
+                                                        <td colspan="4">$age</td>
+                                                </tr>
+                                                <tr>
+                                                        <td>Invite : </td>
+                                                        <td cospan="4">$IC</td>
+                                                </tr>
+                                        </tbody>
+                                </table>
+                                </td>
+                                </tr>
 EOF
 else
 cat <<EOF >> /home/all.html
-				<tr>
-					<td class="abang" colspan="5">Node<h5>$line2</h5><h4>OFF</h4></td>
-				</tr>
+                                <tr>
+                                        <td class="abang" colspan="1">Node</td>
+                                        <td class="abang" colspan="2"><h5>$line2</h5></td>
+                                        <td class="abang" colspan="2"><h4>OFF</h4></td>
+                                </tr>
 EOF
 fi
 done <<<$(cat /home/$line/$line-portRpc.txt)
@@ -128,11 +134,11 @@ done <<<$(cat /home/user.txt)
 cat <<EOF >> /home/all.html
 </tbody>
     </table>
-	<script type="text/javascript"> 
-		function showHideRow(row) { 
-			\$("#" + row).toggle(); 
-		} 
-	</script> 
+        <script type="text/javascript">
+                function showHideRow(row) {
+                        \$("#" + row).toggle();
+                }
+        </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
