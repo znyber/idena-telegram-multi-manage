4a56465626fcf016b5eadcb5cea18269800b7bd2
znyber <pro_army@live.com>
2021-03-14 23:21:11 +0700

    add backup restore tool

---
+++ toolkit/backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while read line; do
+echo $line
+while read line2; do
+NUKE=$(cat /home/$line/$line2/keystore/nodekey)
+echo "$line2"
+echo "$line $NUKE" >> /root/restore.txt
+done <<<$(cat /home/$line/$line-portRpc.txt)
+done <<<$(cat /home/user.txt)
+sort /home/chatidX.txt | uniq -c | awk '{print $2}' > /root/chatidX.txt
+echo "backup complete"
---
+++ toolkit/restore.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+while read line; do
+USER=$(echo "$line" | awk '{print $1}')
+idena-multi $line
+sleep 10
+echo "$USER"
+RPCD=$(tail -n 1 /home/$USER/$USER-portRpc.txt)
+DATC=$(curl -s "http://127.0.0.1:$RPCD/" -H 'Content-Type: application/json' --data "{\"method\":\"bcn_syncing\",\"params\":[],\"id\":1,\"key\":\"$USER\"}")
+SYNCA=$(echo $DATC | sed -n 's|.*"syncing":\([^"]*\),.*|\1|p')
+CURBLOCK=$(echo $DATC | sed -n 's|.*"currentBlock":\([^"]*\),.*|\1|p')
+HIGBLOCK=$(echo $DATC | sed -n 's|.*"highestBlock":\([^"]*\),.*|\1|p')
+SECONDS=0
+while [ "$SYNCA" != false ]
+do
+KOVET=$(netstat -netulp |grep 127.0.0.1:$RPCD | awk -F "[ :]+" '/:/{print $5}')
+if [[ $KOVET == $RPCD ]]
+then
+echo "node $RPCD sync $SYNCA , block curent $CURBLOCK to block high $HIGBLOCK" >&2
+DATC=$(curl -s "http://127.0.0.1:$RPCD/" -H 'Content-Type: application/json' --data "{\"method\":\"bcn_syncing\",\"params\":[],\"id\":1,\"key\":\"$USER\"}")
+SYNCA=$(echo $DATC | sed -n 's|.*"syncing":\([^"]*\),.*|\1|p')
+CURBLOCK=$(echo $DATC | sed -n 's|.*"currentBlock":\([^"]*\),.*|\1|p')
+HIGBLOCK=$(echo $DATC | sed -n 's|.*"highestBlock":\([^"]*\),.*|\1|p')
+BLOCKTIM=$(($HIGBLOCK-$CURBLOCK))
+sleep $(($BLOCKTIM+10))
+else
+sleep 10
+fi
+done
+KOVET=$(netstat -netulp |grep $RPCD | awk -F "[ :]+" '/:/{print $5}')
+if [[ $KOVET == $RPCD ]]
+then
+echo "node $RPCD sync $SYNCA , time to full sync $SECONDS" >&2
+else
+echo "node $RPCD down $KOVET" >&2
+fi
+SECONDS=0
+done <<<$(cat /root/restore.txt)
+cat /root/chatidX.txt >> /home/chatidX.txt
+echo "all restore clear"
